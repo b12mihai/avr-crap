79094d138d2cf54b73773f1f89034234e07b63b1
b12mihai <b12mihai@gmail.com>
2012-06-26 16:23:48 +0300

    created a cooler macro for MAX and MIN

--- lib-stuff/defines.h
+++ lib-stuff/defines.h
@@ -3,5 +3,6 @@
 #define INT_TRIGGER_FALLING	2
 #define INT_TRIGGER_RISING  3
 
-#define MIN(a, b) ((a < b)? a : b)
-#define MAX(a, b) ((a < b)? b : a)
+#define MAXIM(x, y) (x) ^ (((x) ^ (y)) & -((x) < (y)))
+#define MINIM(x, y) (y) ^ (((x) ^ (y)) & -((x) < (y))) 
+
