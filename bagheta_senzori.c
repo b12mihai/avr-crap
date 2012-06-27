5f9135082f88ec79ae0e36e5cee6cc17af147087
b12mihai <b12mihai@gmail.com>
2012-06-27 15:35:23 +0300

    bagheta_senzori.c

--- bagheta_senzori.c
+++ bagheta_senzori.c
@@ -7,30 +7,49 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+uint8_t b0, b1, b2, b3, b4, b5;
+
+void setup_bits(uint8_t bits)
+{
+	b0 = bits & 1;
+	b1 = bits & 2;
+	b2 = bits & 4;
+	b3 = bits & 8;
+	b4 = bits & 16;
+	b5 = bits & 32;
+}
+
+void print_bargraph()
+{
+	PORTA = 0x00;
+	if(b0) 
+		PORTA |= 1;
+	if(b1)
+		PORTA |= (1 << 1);
+	if(b2)
+		PORTA |= (1 << 2);
+	if(b3)
+		PORTA |= (1 << 3);
+	if(b4)
+		PORTA |= (1 << 4);
+	if(b5)
+		PORTA |= (1 << 5);
+}
+	
 int main()
 {
+	DDRA = 0xff;
+	DDRC = 0x00;
 	DDRD = 0xff;
+
+	PORTA = 0x00;
 	PORTD = 0x00;
 
-	DDRA = 0x00;
-	PORTA = 0x00;
-	uint8_t b0, b1, b2, b3, b4, b5, b6;
 
 	while(1) {
-		b0 = PINA & 1;
-		b1 = PINA & (1 << 1);
-		b2 = PINA & (1 << 2);
-		b3 = PINA & (1 << 3);
-		b4 = PINA & (1 << 4);
-		b5 = PINA & (1 << 5);
-		b6 = PINA & (1 << 6);
-
-		if(!b0 && !b1 && b2 && b3 && b4 && !b5 && !b6) {
-			PORTD = 0xff;
-		}
-
-		PORTD = 0x00;
-
+		setup_bits(PINC);
+		print_bargraph();
+		_delay_ms(100);
 	}
 
 	return 0;
